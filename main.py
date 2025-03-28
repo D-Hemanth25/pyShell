3c7d25a4fec0e0df6e51dc0f694a96e8ea6a0601
D-Hemanth25 <mailhemanth25@gmail.com>
2025-03-28 16:04:27 +0530

    Implement the exit builtin

--- main.py
+++ main.py
@@ -5,12 +5,22 @@ def print(content):
     return 0
 
 
+def invalidCommand(command):
+    print(command + ": command not found")
+    return 0
+
+
 def main():
     while True:
         sys.stdout.write("$ ")
-        
+
         command = input()
-        print(command + ": command not found")
+        if command == "exit 0":
+            break
+        else:
+            invalidCommand(command)
+    return 0
+
 
 if __name__ == "__main__":
     main()
