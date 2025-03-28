8aa484bb48b945fa130a955ab178a4a73660721d
D-Hemanth25 <mailhemanth25@gmail.com>
2025-03-28 15:54:37 +0530

    Implement support for handling invalid commands in shell * implemented print function overriding builtin * treat all commands as invalid for now "

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import sys
 
+def print(content):
+    sys.stdout.write(content + "\n")
+    return 0
+
+
 def main():
     sys.stdout.write("$ ")
-    input()
+    
+    command = input()
+    print(command + ": command not found")
 
 if __name__ == "__main__":
     main()
